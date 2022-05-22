9bf0b6dcdd615df3ab3c09ca18304006ce0b4129
LeoMaina <leonardhophni@gmail.com>
2022-05-22 04:40:19 -0700

    Initial commit

--- main.h
+++ main.h
@@ -3,5 +3,8 @@
 
 #include <stdarg.h>
 int _printf(const char *format, ...);
+int print_buffer(char *buf, unsigned int nbuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+unsigned int handle_buffer(char *buf, char c, unsigned int ibuf);
 
 #endif
